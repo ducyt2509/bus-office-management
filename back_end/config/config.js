fb7f78409c8234c192514c1ad977decbf128ac0b
locnthe153667 <73702923+Socquekute@users.noreply.github.com>
2023-02-19 11:22:00 +0700

    ...

--- back_end/config/config.js
+++ back_end/config/config.js
@@ -1,8 +1,4 @@
 require('dotenv').config();
-// const host = process.env.DB_HOST || 'localhost';
-// const user = process.env.DB_USERNAME || 'root';
-// const password = process.env.DB_PASSWORD || '22042001';
-// const dbName = process.env.DB_NAME || 'testdb';
 module.exports = {
   development: {
     username: process.env.DB_USERNAME,
--- back_end/models/index.js
+++ back_end/models/index.js
@@ -31,6 +31,7 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config')[env];
 const db = {};
 
