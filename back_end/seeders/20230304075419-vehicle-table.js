45953b7d6ee1bb03c93f04f65c91e4a6a7b53b21
thanglv <vietthang1118@gmail.com>
2023-03-22 23:26:25 +0700

    Add sample data and Update database

--- back_end/seeders/20230304075419-vehicle-table.js
+++ back_end/seeders/20230304075419-vehicle-table.js
@@ -1,18 +1,18 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('vehicle', [
-      { vehicle_name: '12 chỗ', vehicle_number_seat: 12, office_id: 1 },
-      { vehicle_name: '16 chỗ', vehicle_number_seat: 16, office_id: 1 },
-      { vehicle_name: '29 chỗ', vehicle_number_seat: 29, office_id: 1 },
-      { vehicle_name: '35 chỗ', vehicle_number_seat: 35, office_id: 1 },
-      { vehicle_name: '45 chỗ', vehicle_number_seat: 45, office_id: 1 },
-    ]);
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('vehicle', null, {});
-  },
+	up: (queryInterface, Sequelize) => {
+		return queryInterface.bulkInsert("vehicle", [
+			{ vehicle_name: "12 chỗ", vehicle_number_seat: 12, office_id: 1 },
+			{ vehicle_name: "16 chỗ", vehicle_number_seat: 16, office_id: 1 },
+			{ vehicle_name: "29 chỗ", vehicle_number_seat: 29, office_id: 1 },
+			{ vehicle_name: "35 chỗ", vehicle_number_seat: 35, office_id: 1 },
+			{ vehicle_name: "45 chỗ", vehicle_number_seat: 45, office_id: 1 },
+		]);
+	},
+	down: (queryInterface, Sequelize) => {
+		return queryInterface.bulkDelete("vehicle", null, {});
+	},
 };
