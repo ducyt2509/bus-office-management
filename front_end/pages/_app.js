3144aeb7dd6e0c95cc3a8960945aa846b1295473
thanglv <vietthang1118@gmail.com>
2023-05-01 18:46:10 +0700

    add about us screen

--- front_end/pages/_app.js
+++ front_end/pages/_app.js
@@ -1,34 +1,37 @@
-import '@/styles/globals.css';
-import '@/styles/bom_navbar.css';
-import '@/styles/bom_login.css';
-import '@/styles/bom_home.css';
-import '@/styles/bom_contactus.css';
-import '@/styles/bom_table.css';
-import '@/styles/bom_bus_schedule.css';
-import '@/styles/bom_vehicle_type.css';
-import '@/styles/bom_personal_info.css';
-import '@/styles/bom_toast_position.css';
-import { ChakraProvider } from '@chakra-ui/react';
-import { useRouter } from 'next/router';
-import Sidebar from '@/src/components/common/sidebar/SideBar';
-import NavBar from '@/src/components/home/login';
-import { StoreProvider } from '@/src/store';
-import SideBarDriver from '@/src/components/common/sidebar/SideBarDriver';
+import "@/styles/globals.css";
+import "@/styles/bom_navbar.css";
+import "@/styles/bom_login.css";
+import "@/styles/bom_home.css";
+import "@/styles/bom_contactus.css";
+import "@/styles/bom_table.css";
+import "@/styles/bom_bus_schedule.css";
+import "@/styles/bom_vehicle_type.css";
+import "@/styles/bom_personal_info.css";
+import "@/styles/bom_toast_position.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import Sidebar from "@/src/components/common/sidebar/SideBar";
+import NavBar from "@/src/components/home/login";
+import { StoreProvider } from "@/src/store";
+import SideBarDriver from "@/src/components/common/sidebar/SideBarDriver";
 
 export default function App({ Component, pageProps }) {
-  const router = useRouter();
-  return (
-    <ChakraProvider>
-      <StoreProvider>
-        {(router.pathname.includes('/admin') || router.pathname.includes('/staff')) && <Sidebar />}
-        {router.pathname.includes('/driver') && <SideBarDriver />}
-        {router.pathname != '/contact-us' &&
-          router.pathname != '/' &&
-          !router.pathname.includes('/admin') &&
-          !router.pathname.includes('/staff') &&
-          !router.pathname.includes('/driver') && <NavBar />}
-        <Component {...pageProps} />
-      </StoreProvider>
-    </ChakraProvider>
-  );
+	const router = useRouter();
+	return (
+		<ChakraProvider>
+			<StoreProvider>
+				{(router.pathname.includes("/admin") || router.pathname.includes("/staff")) && (
+					<Sidebar />
+				)}
+				{router.pathname.includes("/driver") && <SideBarDriver />}
+				{router.pathname != "/contact-us" &&
+					router.pathname != "/about-us" &&
+					router.pathname != "/" &&
+					!router.pathname.includes("/admin") &&
+					!router.pathname.includes("/staff") &&
+					!router.pathname.includes("/driver") && <NavBar />}
+				<Component {...pageProps} />
+			</StoreProvider>
+		</ChakraProvider>
+	);
 }
---
+++ front_end/pages/about-us.js
@@ -0,0 +1,6 @@
+import LoginForm from "@/src/components/home/login";
+
+export default function ContactUsHTML() {
+	const LoginFormHTML = <LoginForm />;
+	return <header>{LoginFormHTML}</header>;
+}
