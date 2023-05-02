1975618c4459eb11d32d2322cc5012535cc377c3
phuc <dinhphucnguyen112@gmail.com>
2023-05-03 05:21:14 +0700

    fix transport and responsive

--- front_end/src/components/common/sidebar/NavBar.js
+++ front_end/src/components/common/sidebar/NavBar.js
@@ -1,7 +1,17 @@
 import { useRouter } from "next/router";
 import Link from "next/link";
 import { Box } from "@chakra-ui/react";
+import { useCallback, useState } from "react";
+import { AiOutlineMenu } from "react-icons/ai";
+import { Flex } from "@chakra-ui/react";
+import { color } from "framer-motion";
+
 export default function NavBar(props) {
+	const [menuStatus, setMenuStatus] = useState(false);
+	const handleShowMenu = useCallback(() => {
+		setMenuStatus(!menuStatus);
+	}, [menuStatus]);
+
 	const router = useRouter();
 	return (
 		<div
@@ -12,69 +22,135 @@ export default function NavBar(props) {
 					: {}
 			}
 		>
-			<div className="bom-navbar-bom-logo">
-				<Link href={"/"}>
-					<h1
-						style={
-							router.pathname != "/" && !router.pathname.includes("/contact-us")
-								? { color: "#F26A4C" }
-								: {}
-						}
-					>
-						BOM
-					</h1>
-				</Link>
-			</div>
-			<ul className="bom-navbar-component">
-				<li>
-					<Link href={"/ticket"}>
-						<div
+			<Flex
+				color={"#fff"}
+				justifyContent={"space-between"}
+				flexWrap={"wrap"}
+				width={"100%"}
+				zIndex="2"
+				className="bom-navbar-flex"
+			>
+				<div className="bom-navbar-bom-logo">
+					<Link href={"/"}>
+						<h1
 							style={
 								router.pathname != "/" && !router.pathname.includes("/contact-us")
-									? { color: "#F26A4C" }
+									? { color: "#f26a4c" }
 									: {}
 							}
 						>
-							Tra cứu vé
-						</div>
+							BOM
+						</h1>
 					</Link>
-				</li>
-				<li>
-					<Link href={"/about-us"}>
-						<div
-							style={
-								router.pathname != "/" && !router.pathname.includes("/contact-us")
-									? { color: "#F26A4C" }
-									: {}
-							}
+				</div>
+				<ul className="bom-navbar-component">
+					<li>
+						<Link href={"/ticket"}>
+							<div
+								style={
+									router.pathname != "/" && !router.pathname.includes("/contact-us")
+										? { color: "#F26A4C" }
+										: {}
+								}
+							>
+								Tra cứu vé
+							</div>
+						</Link>
+					</li>
+					<li>
+						<Link href={"/about-us"}>
+							<div
+								style={
+									router.pathname != "/" && !router.pathname.includes("/contact-us")
+										? { color: "#F26A4C" }
+										: {}
+								}
+							>
+								Về chúng tôi
+							</div>
+						</Link>
+					</li>
+					<li>
+						<Link href={"/contact-us"}>
+							<div
+								style={
+									router.pathname != "/" && !router.pathname.includes("/contact-us")
+										? { color: "#F26A4C" }
+										: {}
+								}
+							>
+								Liên hệ
+							</div>
+						</Link>
+					</li>
+					<li>
+						<Box
+							backgroundColor={"#F26A4C"}
+							borderRadius={"md"}
+							onClick={props.handleShowLoginForm}
 						>
-							Về chúng tôi
-						</div>
-					</Link>
-				</li>
-				<li>
-					<Link href={"/contact-us"}>
-						<div
-							style={
-								router.pathname != "/" && !router.pathname.includes("/contact-us")
-									? { color: "#F26A4C" }
-									: {}
-							}
-						>
-							Liên hệ
-						</div>
-					</Link>
-				</li>
-				<li>
+							Đăng nhập
+						</Box>
+					</li>
+				</ul>
+				<Box
+					width="40px"
+					height="40px"
+					textAlign="center"
+					margin="2%"
+					className="boxres"
+				>
+					<AiOutlineMenu
+						fontSize="38px"
+						margin="0 auto"
+						textAlign="center"
+						onClick={handleShowMenu}
+					/>
+				</Box>
+				{menuStatus && (
 					<Box
-						backgroundColor={"#F26A4C"}
-						borderRadius={"md"}
-						onClick={props.handleShowLoginForm}
+						width={"100%"}
+						fontSize={"20px"}
+						textAlign={"end"}
 					>
-						Đăng nhập
+						<ul style={{ width: "90%", margin: "0 auto", listStyle: "none" }}>
+							<li
+								style={{ padding: "4%", borderBottom: "1px solid #fff" }}
+								onClick={() => {
+									router.push("/ticket");
+									setMenuStatus(!menuStatus);
+								}}
+							>
+								Tra cứu vé
+							</li>
+							<li
+								style={{ padding: "4%", borderBottom: "1px solid #fff" }}
+								onClick={() => {
+									router.push("/about-us");
+									setMenuStatus(!menuStatus);
+								}}
+							>
+								Về chúng tôi
+							</li>
+							<li
+								style={{ padding: "4%", borderBottom: "1px solid #fff" }}
+								onClick={() => {
+									router.push("/contact-us");
+									setMenuStatus(!menuStatus);
+								}}
+							>
+								Liên hệ
+							</li>
+							<li
+								style={{ padding: "4%" }}
+								onClick={props.handleShowLoginForm}
+							>
+								Đăng nhập
+							</li>
+						</ul>
 					</Box>
-				</li>
-			</ul>
+				)}
+			</Flex>
 		</div>
 	);
 }
--- front_end/src/components/common/sidebar/SideBarDriver.js
+++ front_end/src/components/common/sidebar/SideBarDriver.js
@@ -1,95 +1,112 @@
-import { Text, Flex, Box, useToast } from '@chakra-ui/react';
-import { AiOutlineMenu } from 'react-icons/ai';
-import { useCallback, useRef, useState } from 'react';
-import { useRouter } from 'next/router';
-import { useStore } from '@/src/store';
+import { Text, Flex, Box, useToast } from "@chakra-ui/react";
+import { AiOutlineMenu } from "react-icons/ai";
+import { useCallback, useRef, useState } from "react";
+import { useRouter } from "next/router";
+import { useStore } from "@/src/store";
 
 export default function SideBarDriver() {
-  const toast = useToast();
-  const toastIdRef = useRef();
-  const [state, dispatch, axiosJWT] = useStore();
-  const router = useRouter();
-  const [menuStatus, setMenuStatus] = useState(false);
-  const handleShowMenu = useCallback(() => {
-    setMenuStatus(!menuStatus);
-  }, [menuStatus]);
+	const toast = useToast();
+	const toastIdRef = useRef();
+	const [state, dispatch, axiosJWT] = useStore();
+	const router = useRouter();
+	const [menuStatus, setMenuStatus] = useState(false);
+	const handleShowMenu = useCallback(() => {
+		setMenuStatus(!menuStatus);
+	}, [menuStatus]);
 
-  const handleLogOut = useCallback(async () => {
-    try {
-      const logOut = await axiosJWT.post(
-        `http://localhost:5000/logout`,
-        { id: state.dataUser.id },
-        {
-          withCredentials: true,
-          headers: { token: `Bearer ${state.dataUser.token}` },
-        }
-      );
-      if (logOut.data.statusCode == 200) {
-        router.push('/');
-      }
-    } catch (err) {
-      console.log(err);
-      if (err.response.data.statusCode == 401) {
-        toastIdRef.current = toast({
-          title: 'Phiên của bạn đã hết hạn.',
-          description: 'Phiên đã hết hạn vui lòng đăng nhập lại.',
-          status: 'error',
-          isClosable: true,
-          position: 'top',
-          duration: 2000,
-        });
-      } else {
-        toastIdRef.current = toast({
-          title: err.response.data.data.message,
-          description: 'Không thể đăng xuất. Làm ơn hãy thử lại.',
-          status: 'error',
-          isClosable: true,
-          position: 'top',
-          duration: 2000,
-        });
-      }
-    }
-  }, [state]);
+	const handleLogOut = useCallback(async () => {
+		try {
+			const logOut = await axiosJWT.post(
+				`http://localhost:5000/logout`,
+				{ id: state.dataUser.id },
+				{
+					withCredentials: true,
+					headers: { token: `Bearer ${state.dataUser.token}` },
+				},
+			);
+			if (logOut.data.statusCode == 200) {
+				router.push("/");
+			}
+		} catch (err) {
+			console.log(err);
+			if (err.response.data.statusCode == 401) {
+				toastIdRef.current = toast({
+					title: "Phiên của bạn đã hết hạn.",
+					description: "Phiên đã hết hạn vui lòng đăng nhập lại.",
+					status: "error",
+					isClosable: true,
+					position: "top",
+					duration: 2000,
+				});
+			} else {
+				toastIdRef.current = toast({
+					title: err.response.data.data.message,
+					description: "Không thể đăng xuất. Làm ơn hãy thử lại.",
+					status: "error",
+					isClosable: true,
+					position: "top",
+					duration: 2000,
+				});
+			}
+		}
+	}, [state]);
 
-  return (
-    <Flex
-      backgroundColor="#F26A4C"
-      color={'#fff'}
-      justifyContent={'space-between'}
-      flexWrap={'wrap'}
-      position="fixed"
-      width={'100%'}
-      zIndex="2"
-    >
-      <Text fontFamily="Gugi" fontSize="38px" margin={'1% 5%'}>
-        BOM
-      </Text>
-      <Box width="40px" height="40px" textAlign="center" margin="2%">
-        <AiOutlineMenu
-          fontSize="38px"
-          margin="0 auto"
-          textAlign="center"
-          onClick={handleShowMenu}
-        />
-      </Box>
-      {menuStatus && (
-        <Box width={'100%'} fontSize={'20px'} textAlign={'end'} paddingBottom={'10px'}>
-          <ul style={{ width: '90%', margin: '0 auto', listStyle: 'none' }}>
-            <li
-              style={{ padding: '4%', borderBottom: '1px solid #fff' }}
-              onClick={() => {
-                router.push('/driver/settings/user-information');
-                setMenuStatus(!menuStatus);
-              }}
-            >
-              Thông tin cá nhân
-            </li>
-            <li style={{ padding: '4%' }} onClick={handleLogOut}>
-              Đăng xuất
-            </li>
-          </ul>
-        </Box>
-      )}
-    </Flex>
-  );
+	return (
+		<Flex
+			backgroundColor="#F26A4C"
+			color={"#fff"}
+			justifyContent={"space-between"}
+			flexWrap={"wrap"}
+			position="fixed"
+			width={"100%"}
+			zIndex="2"
+		>
+			<Text
+				fontFamily="Gugi"
+				fontSize="38px"
+				margin={"1% 5%"}
+			>
+				BOM
+			</Text>
+			<Box
+				width="40px"
+				height="40px"
+				textAlign="center"
+				margin="2%"
+			>
+				<AiOutlineMenu
+					fontSize="38px"
+					margin="0 auto"
+					textAlign="center"
+					onClick={handleShowMenu}
+				/>
+			</Box>
+			{menuStatus && (
+				<Box
+					width={"100%"}
+					fontSize={"20px"}
+					textAlign={"end"}
+					paddingBottom={"10px"}
+				>
+					<ul style={{ width: "90%", margin: "0 auto", listStyle: "none" }}>
+						<li
+							style={{ padding: "4%", borderBottom: "1px solid #fff" }}
+							onClick={() => {
+								router.push("/driver/settings/user-information");
+								setMenuStatus(!menuStatus);
+							}}
+						>
+							Thông tin cá nhân
+						</li>
+						<li
+							style={{ padding: "4%" }}
+							onClick={handleLogOut}
+						>
+							Đăng xuất
+						</li>
+					</ul>
+				</Box>
+			)}
+		</Flex>
+	);
 }
