50138f70f65250fbfca2758b98e9a324c1fbbd24
thanglv <vietthang1118@gmail.com>
2023-05-03 12:07:19 +0700

    fix error when book ticket

--- back_end/routes/ticket.js
+++ back_end/routes/ticket.js
@@ -1,8 +1,9 @@
-const middleWare = require('../middleware/permission.middleware');
+const middleWare = require("../middleware/permission.middleware");
 
-const tickets = require('../controllers').ticket;
-var router = require('express').Router();
+const tickets = require("../controllers").ticket;
+var router = require("express").Router();
 
-router.post('/revenue/list-revenue', middleWare.verifyTokenForManager, tickets.getRevenueList);
+router.post("/revenue/list-revenue", middleWare.verifyTokenForManager, tickets.getRevenueList);
+router.post("/ticket/create-ticket", middleWare.verifyTokenForDriver, tickets.createNewTicket);
 
 module.exports = router;
