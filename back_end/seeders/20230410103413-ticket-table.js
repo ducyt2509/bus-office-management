6133b0e4a0c6b44799d2f935a4b46e880e255cdd
thanglv <vietthang1118@gmail.com>
2023-05-04 06:21:58 +0700

    add data

--- back_end/seeders/20230410103413-ticket-table.js
+++ back_end/seeders/20230410103413-ticket-table.js
@@ -1,23 +1,24 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('ticket', [{
-      transaction_id: 4
-    },
-    {
-      transaction_id: 5
-    },
-    {
-      transaction_id: 6
-    }
-
-
-
-    ]);
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('Users', null, {});
-  }
+	up: (queryInterface, Sequelize) => {
+		return queryInterface.bulkInsert("ticket", [
+			{
+				transaction_id: 1,
+			},
+			{
+				transaction_id: 2,
+			},
+			{
+				transaction_id: 3,
+			},
+			{
+				transaction_id: 4,
+			},
+		]);
+	},
+	down: (queryInterface, Sequelize) => {
+		return queryInterface.bulkDelete("Users", null, {});
+	},
 };
